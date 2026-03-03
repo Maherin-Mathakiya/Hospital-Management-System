e1e06c5b2e6bddc298b6ffc8260f580a19e5a16a
Maherin-Mathakiya <mathakiyamaherin10@gmail.com>
2026-03-03 22:01:23 +0530

    Add files via upload

---
+++ Models/DoctorModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HMS.Models
+{
+    public class DoctorModel
+    {
+        public int DoctorID { get; set; }
+        
+        [Required(ErrorMessage ="Required Name")]
+        [StringLength(maximumLength:20, ErrorMessage ="Not valid name", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Required Phone Number")]
+        [Phone(ErrorMessage ="Not valid Number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage ="Required Email Address")]
+        [EmailAddress(ErrorMessage ="Enter valid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Required Qualification")]
+        [StringLength(100, MinimumLength =4, ErrorMessage = "Enter valid Qualification")]
+        public string Qualification { get; set; }
+
+        [Required(ErrorMessage = "Required Specialization")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Enter valid Specialization")]
+        public string Specialization { get; set; }
+       
+        public bool IsActive { get; set; }
+        public DateTime Modified { get; set; } = DateTime.Now;
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Required UserID")]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "Required User Name")]
+        public string UserName { get; set; }
+
+    }
+
+    public class DoctorDropDownModel
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+    }
+}
